77144b445cec54f5154af01fa1422ee11194b873
sumeetsrivastav0728-coder <sumeetsrivastav0728@gmail.com>
2026-02-25 02:39:03 +0530

    Polished CLI commands and handled edge cases

--- index.js
+++ index.js
@@ -7,12 +7,16 @@ if(!command){
     process.exit(0);
 }
 if(command==="add"){
+    if(!argument){
+        console.log("Please provide a note to add");
+        process.exit(0);
+    }
     addNote(argument);
     console.log("Note added successfully");
 }
 else if(command==='list'){
     listNotes();}
     else {
-        console.log("Unknown command");
+        console.log("Unknown command.Available commands:add,list");
     }
 
